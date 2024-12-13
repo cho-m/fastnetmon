325fa8548cdf95161fd520c62be2e3e9c92f4c1e
Michael Cho <michael@michaelcho.dev>
2024-12-12 21:24:28 -0500

    Added support for Boost 1.87.0

--- src/fast_library.cpp
+++ src/fast_library.cpp
@@ -1094,14 +1094,11 @@ std::string serialize_network_load_to_text(subnet_counter_t& network_speed_meter
 
 std::string dns_lookup(std::string domain_name) {
     try {
-        boost::asio::io_service io_service;
-        boost::asio::ip::tcp::resolver resolver(io_service);
+        boost::asio::io_context ioc;
+        boost::asio::ip::tcp::resolver resolver(ioc);
 
-        boost::asio::ip::tcp::resolver::query query(domain_name, "");
-
-        for (boost::asio::ip::tcp::resolver::iterator i = resolver.resolve(query);
-             i != boost::asio::ip::tcp::resolver::iterator(); ++i) {
-            boost::asio::ip::tcp::endpoint end = *i;
+        for (const auto& entry : resolver.resolve(domain_name, "")) {
+            boost::asio::ip::tcp::endpoint end = entry.endpoint();
             return end.address().to_string();
         }
     } catch (std::exception& e) {
@@ -1428,7 +1425,7 @@ bool validate_ipv6_or_ipv4_host(const std::string host) {
     boost::system::error_code ec;
 
     // Try to build it from string representation
-    boost::asio::ip::address::from_string(host, ec);
+    boost::asio::ip::make_address(host, ec);
 
     // If we failed to parse it
     if (ec) {
@@ -1535,7 +1532,7 @@ bool execute_web_request_secure(std::string address,
             return false;
         }
 
-        auto end_point = resolver.resolve(boost::asio::ip::tcp::resolver::query{ host, port }, ec);
+        auto end_point = resolver.resolve(host, port, ec);
 
         if (ec) {
             logger << log4cpp::Priority::ERROR << "Could not resolve peer address in execute_web_request " << ec;
@@ -1699,11 +1696,11 @@ bool execute_web_request(std::string address,
 
         // Normal boost::asio setup
         // std::string const host = "178.62.227.110";
-        boost::asio::io_service ios;
-        boost::asio::ip::tcp::resolver r(ios);
-        boost::asio::ip::tcp::socket sock(ios);
+        boost::asio::io_context ioc;
+        boost::asio::ip::tcp::resolver r(ioc);
+        boost::asio::ip::tcp::socket sock(ioc);
 
-        auto end_point = r.resolve(boost::asio::ip::tcp::resolver::query{ host, port }, ec);
+        auto end_point = r.resolve(host, port, ec);
 
         if (ec) {
             error_text = "Could not resolve peer address in execute_web_request " + ec.message();
